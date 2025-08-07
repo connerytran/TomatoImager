920439d6a04cacb3266be0e8546e379bcbccf4a3
Connery Tran <chtran3@ncsu.edu>
2025-08-07 14:27:29 -0400

    working flask server

--- flask/pi_api.py
+++ flask/pi_api.py
@@ -1,9 +1,9 @@
-import os
+# import os
 from flask import Flask, jsonify
 import subprocess
 from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # src_dir_path = os.getenv('src_dir_path')
 
@@ -11,6 +11,10 @@ src_dir_path = "/home/tomato-imager/TomatoImager/src/"
 venv_python = "/home/tomato-imager/TomatoImager/venv/bin/python"
 
 app = Flask(__name__)
+
+
+
+
 
 
 # Retrieves the status of the pi
@@ -21,27 +25,39 @@ def get_status():
   return jsonify(status)
 
 
+
+
+
+
 # Begins taking pics
 @app.route('/take_pictures', methods=['POST'])
 def take_pictures():
 
   command = [venv_python, 'usb_cam.py'] #first arg ensures our venv is the py environment used
 
-  message = {"Aint work gang"}
   try:
-    subprocess.run(command, cwd=src_dir_path, capture_output=True, text=True, check=True)
+    subprocess.run(command, cwd=src_dir_path, text=True, check=True)
     message = {"message": "SUCCESS, taking pictures now..."}
-    
+    return jsonify(message)
   except subprocess.CalledProcessError as e:
-    print(f"Error executing Globus transfer: {e}")
-    print("STDOUT:", e.stdout)
-    print("STDERR:", e.stderr)
+    message = {
+      "message": "ERROR: The script failed to execute.",
+      "status": "error",
+      "details": e.stderr
+    }
+    return jsonify(message), 500
   except FileNotFoundError:
-    print("Error: 'globus' command not found. Is Globus CLI installed and in PATH?")
+    message = {
+      "message": "ERROR: File not found.",
+      "status": "error",
+    }    
+    return jsonify(message), 500
   except Exception as e:
-    print(f"An unexpected error occurred: {e}")
-
-  return jsonify(message)
+    message = {
+      "message": "ERROR: Unexpected error.",
+      "status": "error",
+    }    
+    return jsonify(message), 500
 
 
 
@@ -52,22 +68,29 @@ def stop_pictures():
 
   command = [venv_python, 'stop_sig.py'] #first arg ensures our venv is the py environment used
 
-  message = {"Aint work gang"}
   try:
-    subprocess.run(command, cwd=src_dir_path, capture_output=True, text=True, check=True)
-    message = {"message": "SUCCESS, taking pictures now..."}
-    
+    subprocess.run(command, cwd=src_dir_path, text=True, check=True)
+    message = {"message": "SUCCESS, stopping now..."}
+    return jsonify(message)
   except subprocess.CalledProcessError as e:
-    print(f"Error executing Globus transfer: {e}")
-    print("STDOUT:", e.stdout)
-    print("STDERR:", e.stderr)
+    message = {
+      "message": "ERROR: The script failed to execute.",
+      "status": "error",
+      "details": e.stderr
+    }
+    return jsonify(message), 500
   except FileNotFoundError:
-    print("Error: 'globus' command not found. Is Globus CLI installed and in PATH?")
+    message = {
+      "message": "ERROR: File not found.",
+      "status": "error",
+    }    
+    return jsonify(message), 500
   except Exception as e:
-    print(f"An unexpected error occurred: {e}")
-
-
-  return jsonify(message)
+    message = {
+      "message": "ERROR: Unexpected error.",
+      "status": "error",
+    }    
+    return jsonify(message), 500
 
 
 
